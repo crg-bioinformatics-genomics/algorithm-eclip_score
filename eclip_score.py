e1a757f52f52d3300d882ccc4ef3c3e1c4c0f57a
Alexandros Armaos <alexandrosarmaos@dgkqr098gf1l.crg.es>
2016-10-10 16:40:00 +0200

    First commit

--- eclip_score.py
+++ eclip_score.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import shutil, errno
 import sys
+import json
 
 # we want to be agnostic to where the script is ran
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -12,25 +13,25 @@ WORKER_PATH = os.path.realpath(os.curdir)
 
 # Function to copy the output directory content
 def copyfolder(src, dst):
-    try:
-        shutil.copytree(src, dst)
-    except OSError as exc: # python >2.5
-        if exc.errno == errno.ENOTDIR:
-            shutil.copy(src, dst)
-        else: raise
+	try:
+		shutil.copytree(src, dst)
+	except OSError as exc: # python >2.5
+		if exc.errno == errno.ENOTDIR:
+			shutil.copy(src, dst)
+		else: raise
 
 # read the task definition yaml file
-with open(os.path.join(SCRIPT_PATH, "catrapid_fragments_ultra.yaml"), "r") as task_f:
+with open(os.path.join(SCRIPT_PATH, "eclip_score.yaml"), "r") as task_f:
 	task_definition = yaml.load(task_f)
 
 parser = argparse.ArgumentParser(
-   description='Launches catRAPID fragments with properly parset parameters')
+   description='Launches')
 
 # parser.add_argument(
-#    '-fileA', type=str, default=["none"], nargs=1, help='Dataset A')
+#	'-fileA', type=str, default=["none"], nargs=1, help='Dataset A')
 #
 # parser.add_argument(
-#    '-fileB', type=str, default=["none"], nargs=1, help='Dataset B')
+#	'-fileB', type=str, default=["none"], nargs=1, help='Dataset B')
 
 parser.add_argument(
    '-output_dir', type=str, nargs=1,
@@ -40,8 +41,8 @@ parser.add_argument(
 for item in task_definition['form_fields']:
    nargs = 1 if item['required'] else "?"
    parser.add_argument(
-       '-FORM%s'%item['name'], type=str, default=["none"], nargs=nargs,
-       help='Form argument: %s' % item)
+	   '-FORM%s'%item['name'], type=str, default=["none"], nargs=nargs,
+	   help='Form argument: %s' % item)
 
 # this parse_args stops if any unexpected arguments is passed
 args = parser.parse_args()
@@ -68,17 +69,7 @@ output_handle = open(protFile, "w")
 SeqIO.write(protSeq, output_handle, "fasta")
 output_handle.close()
 
-Rpat = re.compile('>.*?\n[GATCU]+', re.IGNORECASE)
-if Rpat.match(args.FORMrna_seq[0]) == None:
-	args.FORMrna_seq[0] = ">input_rna\n"+args.FORMrna_seq[0]
-rnaSeq = []
-for record in SeqIO.parse(StringIO.StringIO(args.FORMrna_seq[0]), "fasta"):
-	rnaSeq.append(record)
 
-rnaFile = os.path.join(OUTPUT_PATH.replace("output/", ""),"rna.fasta")
-output_handle = open(rnaFile, "w")
-SeqIO.write(rnaSeq, output_handle, "fasta")
-output_handle.close()
 
 os.chdir(SCRIPT_PATH)
 # print(WORKER_PATH)
@@ -95,9 +86,7 @@ else:
 	title = args.FORMtitle.replace(" ", "_")
 
 logfile = open("pylog."+str(random_number)+".txt","w")
-
-# cmd = """bash runcatrapid.fragments.sh "{}" "{}" "{}" "{}" "{}" "{}" "{}" """.format(random_number, args.FORMemail, title, args.FORMprotFrag[0], "150", os.path.join(WORKER_PATH,args.fileA[0].replace(' ', '-')), os.path.join(WORKER_PATH,args.fileB[0].replace(' ', '-')) )
-cmd = """bash eclip_score.sh "{}" "{}" "{}" "{}" "{}" "{}" "{}" "{}" """.format(random_number, args.FORMemail[0], title, args.FORMprotFrag[0], "150", protFile, rnaFile, args.FORMextras[0])
+cmd = """bash eclip_score.sh "{}" "{}" "{}" "{}" "{}"  """.format(random_number, args.FORMemail[0], title, "150", protFile)
 
 p = subprocess.Popen(cmd, cwd=SCRIPT_PATH, shell=True)
 
@@ -113,7 +102,7 @@ if p.returncode == 0:
 	logfile.write(TMP_PATH+"\n")
 	dirList=os.listdir(TMP_PATH)
 	shutil.copyfile(os.path.join(OUTPUT_PATH.replace("output/", ""),"protein.fasta"), OUTPUT_PATH+"protein.fasta")
-	shutil.copyfile(os.path.join(OUTPUT_PATH.replace("output/", ""),"rna.fasta"), OUTPUT_PATH+"rna.fasta")
+
 	logfile.write("copied fastas\n")
 	for file in dirList:
 		if os.path.isfile(TMP_PATH+file): #ignore directories
@@ -130,19 +119,12 @@ if p.returncode == 0:
 
 	settings.configure(TEMPLATE_DIRS=(os.path.join(SCRIPT_PATH,'./')), DEBUG=True, TEMPLATE_DEBUG=True)
 
-	logfile.write(args.FORMprotFrag[0]+"\n")
-	if args.FORMprotFrag[0] == "uniform":
-	#	# read the template file into a variable
-		with open(os.path.join(OUTPUT_PATH, "index.mat.tmp.html"), "r") as template_file:
-			template_string = "".join(template_file.readlines())
-		logfile.write("copied index.mat.tmp.html\n")
 
-	if args.FORMprotFrag[0] == "weighted":
-		with open(os.path.join(OUTPUT_PATH, "index.Z.tmp.html"), "r") as template_file:
-			template_string = "".join(template_file.readlines())
-		logfile.write("copied index.Z.tmp.html\n")
 
 	import datetime
+	with open(os.path.join(SCRIPT_PATH, "index.mat.html"), "r") as template_file:
+		template_string = "".join(template_file.readlines())
+	logfile.write("copied index.mat.tmp.html\n")
 
 	# create template from the string
 	t = Template(template_string)
@@ -153,13 +135,23 @@ if p.returncode == 0:
 			"title": title,
 			"rnaFrag" : "150",
 			"randoms" : random_number,
-# 			"fileA" : protFile,
-# 			"fileB" : rnaFile,
+#			 "fileA" : protFile,
+#			 "fileB" : rnaFile,
 			"generated" : str(datetime.datetime.now()),
 		}
 	)
 
 	# and this bit outputs it all into index.html
+	rnas=[]
+	values=[]
+	with open(os.path.join(OUTPUT_PATH,"filter.processed.txt"), "r") as fltr:
+		for line in fltr:
+			rnas.append(line.split()[0].strip())
+			values.append(line.split()[1].strip())
+	fltr.close()
+	with os.path.join(OUTPUT_PATH,"transcript.rows"),'w') as fobj:
+		json.dump(zip(rnas,values),fobj)
+	fobj.close()
 	with open(os.path.join(OUTPUT_PATH, "index.html"), "w") as output:
 		output.write(t.render(c))
 	logfile.write("created index.html\n")
--- eclip_score.sh
+++ eclip_score.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# bash runcatrapid.fragments.sh <random_number> <email_address> <title> <type> <sizeRNAs> <protein file> <RNA file> <extras>
-echo $1 $2 $3 $4 $5 $6 $7 $8
+# bash eclip_score.sh <random_number> <email_address> <title> <type> <protein file> 
+echo $1 $2 $3 $4 $5
 
 set -e
 #set -o pipeline
@@ -13,52 +13,31 @@ cd   tmp/$1
 
 # modifies fasta files into 2 columns
 # memo: outfile is the protein file
-./flip -u $6
-sed 's/[\| | \\ | \/ | \* | \$ | \# | \? | \! ]/./g' $6 | awk '(length($1)>=1)' | awk '($1~/>/){gsub(" ", "."); printf "\n%s\t", $1} ($1!~/>/){printf "%s", toupper($1)}' | awk '(NF==2)' | head -1 | sed 's/>\.//g;s/>//g' | awk '{print substr($1,1,12)"_"NR, toupper($2)}' >  ./protein/outfile
-./flip -u $7
-sed 's/[\| | \\ | \/ | \* | \$ | \# | \? | \! ]/./g' $7 | awk '(length($1)>=1)' | awk '($1~/>/){gsub(" ", "."); printf "\n%s\t", $1} ($1!~/>/){gsub(/[Uu]/, "T", $1); printf "%s",toupper($1)}' | awk '(NF==2)' | head -1 | sed 's/>\.//g;s/>//g' | awk '{print substr($1,1,12)"_"NR, $2}' >  ./rna/outfile2
+./flip -u $5
+sed 's/[\| | \\ | \/ | \* | \$ | \# | \? | \! ]/./g' $5 | awk '(length($1)>=1)' | awk '($1~/>/){gsub(" ", "."); printf "\n%s\t", $1} ($1!~/>/){printf "%s", toupper($1)}' | awk '(NF==2)' | head -1 | sed 's/>\.//g;s/>//g' | awk '{print substr($1,1,12)"_"NR, toupper($2)}' >  ./protein/outfile
 
-if [[ ! -s "./protein/outfile" || ! -s "./rna/outfile2" ]]; then
-        echo "A file has not been created."
+if [[ ! -s "./protein/outfile" ]]; then
+        echo "Protein file has not been created."
 	exit 1
 fi
 
 echo "# Reference Sequences" >  ./outputs/yoursequences.$1.$3.txt
 cat ./protein/outfile  	    >>  ./outputs/yoursequences.$1.$3.txt
-cat ./rna/outfile2	        >>  ./outputs/yoursequences.$1.$3.txt
 
-num1=`cat ./rna/outfile2     | awk '{print NR}'`
+
 num2=`cat ./protein/outfile  | awk '{print NR}'`
 
-echo "sequence processing and fragmentation"
+echo "Protein sequence processing and fragmentation"
 date +"%m-%d-%y %r"
 
-cd ./rna
-
-	echo "# SERVER RUNNING...PLEASE REMEMBER TO REFRESH THIS PAGE FROM TIME TO TIME..." >  ../outputs/library.$1.$3.txt;
-	nt=`cat outfile2           | awk '(NR==1){s=length($2)/50; if(s<=25){s=25} if(s>=750){s=750} print int(s)}'`
-	bash ../fragmentation/cutter.sh outfile2 $nt >  outfile2.fr
-cd ../
-
-cd protein
-	pr=`cat ../protein/outfile | awk '(NR==1){s=length($2)/50; if(s<=25){s=25} if(s>=375){s=375} print int(s)}'`
-	bash ../fragmentation/cutter.sh outfile  $pr >  outfile.fr
+cp protein/outfile fragmentation/outfile
+cd fragmentation
+	bash protein.job.cutter.sh outfile > ../protein/outfile.fr
 cd ../
 
 	# RNA LIBRARY GENERATION ######################################################################################
-echo "protein and RNA libraries generation"
+echo "Protein library generation"
 date +"%m-%d-%y %r"
-	# run cases one by one
-cd rna.libraries.U
-	cd rna_library_generator/
-		sed 's/|/_/g' ../../rna/outfile2.fr > outfile2.fr.txt
-		bash rungenerator.rna.sh outfile2.fr.txt
-		mv outfile2.fr.rna.lib ../../outputs/rna.library.$1.$3.txt
-		rm outfile2.fr.txt
-	cd ../
-cd ../
-
-wc rna/outfile2.fr  | awk '($1==0){print "Sorry! no fragments found!"}' >> outputs/library.$1.$3.txt
 
 cd protein.libraries.U
 	cd protein_library_generator/
@@ -77,17 +56,35 @@ date +"%m-%d-%y %r"
 
 cd interactions.U/
 	cp ../outputs/protein.library.$1.$3.txt combine_parallel/prot/prot.lib
-	cp ../outputs/rna.library.$1.$3.txt combine_parallel/rna/rna.lib
 
 		cd combine_parallel/
-			mkdir pre-compiled/
-			bash parallelize.sh 8 prot/prot.lib rna/rna.lib
+			if [ ! -s pre-compiled ]; then
+				mkdir pre-compiled/
+			fi
+
+			python multiplier.py 10 "prot" "lincrnas"
 			echo "# protein / rna / raw score / dp " > ../../outputs/interactions.$1.$3.txt
-			cat pre-compiled/out.merged.txt >> ../../outputs/interactions.$1.$3.txt
-			rm prot/prot.lib rna/rna.lib
+			cat pre-compiled/* >> ../../outputs/interactions.$1.$3.txt
+			#cat pre-compiled/* > pre-compiled/out.merged.txt
+		cd ../../
+
+	cd filter
+
+			echo "Global Score computing"
+			date +"%m-%d-%y %r"
+
+			for i in `ls ../interactions.U/combine_parallel/pre-compiled/`; do
+				prot_rna=$(echo $i | awk -F '.' '{print $3}')
+				rna=$(echo $prot_rna | awk -F '-' '{print $2}')
+				awk '{print "protein_"$1,"rna_"$2,$3,$4}' ../interactions.U/combine_parallel/pre-compiled/$i > interactions.$prot_rna.txt
+				bash start.sh interactions.$prot_rna.txt > $prot_rna.processed.txt
+				echo $rna $(awk '{printf "%.2f\n", ($2+1)/2}' $prot_rna.processed.txt) >> ../outputs/filter.processed.txt
+			done
+
 
 		cd ..
-cd ..
+
+
 
 
 wc protein/outfile.fr  | awk '($1==0){print "Sorry! no fragments found!"}' >> outputs/interactions.$1.$3.txt
@@ -95,29 +92,7 @@ wc protein/outfile.fr  | awk '($1==0){print "Sorry! no fragments found!"}' >> ou
 
 
 
-########################
-####### FILTER #########
-########################
 
-
-##### filter for fragments
-
-## N.B. With UNIFORM option, IDs are removed and just fragment coordinates are shown;
-## For this reason, we need to add "protein_" and "rna_" before the coordinates.
-
-cd filter
-
-if [[ "$4" == "uniform" ]]; then
-
-	echo "Global Score computing"
-	date +"%m-%d-%y %r"
-
-	awk '{print "protein_"$1,"rna_"$2,$3,$4}' ../interactions.U/combine_parallel/pre-compiled/out.merged.txt > interactions.$1.$3.txt
-	bash start.sh interactions.$1.$3.txt -1 > processed.txt
-	awk '{printf "%.2f\n", ($2+1)/2}' processed.txt > ../outputs/filter.tmp
-fi
-
-cd ..
 
 
 ######################################
@@ -128,26 +103,7 @@ cd ..
 echo "statistics and table"
 date +"%m-%d-%y %r"
 
-if [[ "$4" == "weighted" ]] || [[ "$8" != "signalLoc" ]]; then
-cd ./statistics
 
-	if [ "$opt" = "1" ] ; then # uniform
-		bash bin/run.2.sh $1 $3
-	fi
-
-	if [ "$opt" = "0" ] ; then # weighted
-		bash bin/run.4.sh $1 $3
-	fi
-	# rm cases
-	# rm *pdf
-
-cd ..
-awk 'BEGIN{printf "<tbody>\n"}{printf "\t<tr>\n\t\t<td>%d</td>\n\t\t<td>%s</td>\n\t\t<td>%s</td>\n\t\t<td>%.2f</td>\n\t\t<td>%d</td>\n\t\t<td>%.2f</td>\n\t</tr>\n", $1, $2, $3, $4, $5*100, $6}END{print """</tbody>"""}' outputs/best.prot.rna.txt > outputs/statistics.$1.$3.html
-fi
-
-if [[ "$4" == "weighted" ]]; then
-php ../../index.Z.html > outputs/index.Z.tmp.html
-fi
 if [[ "$4" == "uniform" ]]; then
 php ../../index.mat.html > outputs/index.mat.tmp.html
 fi
